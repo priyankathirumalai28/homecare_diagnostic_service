42b95f22947a092c0fb1e441c02a83906b960901
priyankathirumalai <priyanka.thirumalai28@gmail.com>
2021-10-06 20:28:46 +0530

    updated the buttons

--- static/js/homecare.js
+++ static/js/homecare.js
@@ -1,8 +1,37 @@
-<script type="text/javascript">
-     $(function () {
-         $('#datetimepicker1').datetimepicker();
-     });
-     $(':radio').change(function() {
+$(':radio').change(function() {
   console.log('New star rating: ' + this.value);
     });
-</script>
+    $(".service").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".services").offset().top
+    }, 2000);   
+});
+$(function() {
+  $('#datepicker').datetimepicker({
+  minDate:new Date(),
+  format: 'YYYY-MM-DD',
+  });
+});
+$(function() {
+  $('#timepicker').datetimepicker({
+format: 'LT'
+  });
+});
+$(document).ready(function() {
+    $('#book_appointment').click(function(e) {
+    e.preventDefault();
+        $('html, body').animate({
+          scrollTop: $("#book_appointment_form").offset().top
+        }, "fast");
+    });
+});
+$(document).ready(function() {
+    $('#service').click(function(e) {
+    e.preventDefault();
+        $('html, body').animate({
+          scrollTop: $(".services").height()
+        }, "fast");
+    });
+});
+
+$('#basicModal').modal('show');

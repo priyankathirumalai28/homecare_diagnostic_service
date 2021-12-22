d7a30c57c24ac02794b61bd42e531c31491cdc84
priyankathirumalai <priyanka.thirumalai28@gmail.com>
2021-12-22 18:02:56 +0530

    updated upto services.html

--- static/js/homecare.js
+++ static/js/homecare.js
@@ -1,11 +1,3 @@
-$(':radio').change(function() {
-  console.log('New star rating: ' + this.value);
-    });
-    $(".service").click(function() {
-    $('html, body').animate({
-        scrollTop: $(".services").offset().top
-    }, 2000);   
-});
 $(function() {
   $('#datepicker').datetimepicker({
   minDate:new Date(),
@@ -17,21 +9,14 @@ $(function() {
 format: 'LT'
   });
 });
-$(document).ready(function() {
-    $('#book_appointment').click(function(e) {
-    e.preventDefault();
-        $('html, body').animate({
-          scrollTop: $("#book_appointment_form").offset().top
-        }, "fast");
-    });
-});
-$(document).ready(function() {
-    $('#service').click(function(e) {
-    e.preventDefault();
-        $('html, body').animate({
-          scrollTop: $(".services").height()
-        }, "fast");
-    });
-});
 
-$('#basicModal').modal('show');
+// for scroll down to the Services list div
+$(document).ready(function() {
+          $('a[href^="#service"]').click(function() {
+          var hash = this.hash;
+              $('html, body').animate({
+                  scrollTop:  $(hash).offset().top
+                }, 500);
+              return false;
+            });
+        });

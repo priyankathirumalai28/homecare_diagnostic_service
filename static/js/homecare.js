2963409f714675ee1aac06df6aa9ca8b1193f37c
priyankathirumalai <priyanka.thirumalai28@gmail.com>
2021-12-29 15:07:25 +0530

    top section updates

--- static/js/homecare.js
+++ static/js/homecare.js
@@ -20,4 +20,21 @@ $(document).ready(function() {
               return false;
             });
         });
+$(document).ready(function () {
+    $("#hcds_appointment").click(function(){
+         $('#appointment_modal').modal('show');
+    });
+    $("#hcds_feedback").click(function(){
+         $('#feedbackmodel').modal('show');
+    });
+     $("#hcds_call_request").click(function(){
+         $('#call_back_request_model').modal('show');
+    });
+    $("#hcds_package").click(function(){
+         $('#package_modal').modal('show');
+    });
+    $("#hcds_offer").click(function(){
+         $('#offer_modal').modal('show');
+    });
+});
 
